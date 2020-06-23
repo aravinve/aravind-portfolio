9186b027bd9fc5a7ef951ca27eb88dfee573796c
Aravind Venkatesan <aravindtherocker@gmail.com>
2020-06-23 22:42:58 +0800

    add carousel

--- js/resume.js
+++ js/resume.js
@@ -35,77 +35,93 @@
 
   $('#modal-close-btn').click(function () {
     $('#projectModalLabel').text('');
-    $('.modal-body').text('');
+    $('img[class="d-block w-100"]').each(function (index, element) {
+      $(element).attr('src', '#!');
+    });
+    $('.modal-body .content').text('');
     $('#github_uri').html('Link');
     $('#github_uri').attr('href', '#!');
-    $('#demo_uri').html('Link');
-    $('#demo_uri').attr('href', '#!');
   });
 
   $('#rv-labs').click(function () {
-    $('#projectModalLabel').text('RV LABS');
-    $('.modal-body').text(
+    $('#projectModalLabel').html(
+      '<img class="rounded my-thumb" src="./img/project/1_F.png" alt="Card image cap"> RV LABS'
+    );
+    $('img[class="d-block w-100"]').each(function (index, element) {
+      $(element).attr('src', `./img/gif/rv-labs/${index + 1}.png`);
+    });
+    $('.modal-body .content').text(
       'Worked as a freelancer for an established pharmaceutical firm.Designed & Developed a responsive business website using HTML5, CSS,JavaScript, Figma & Bootstrap.'
     );
     $('#github_uri').html('<i class="fab fa-github"> </i> Github');
     $('#github_uri').attr('href', 'https://github.com/aravinve/rv-labs');
-    $('#demo_uri').html('<i class="fas fa-eye"></i> Live Demo');
-    $('#demo_uri').attr('href', '#!');
   });
   $('#rovereatz').click(function () {
-    $('#projectModalLabel').text('Rovereatz');
-    $('.modal-body').text(
+    $('#projectModalLabel').html(
+      '<img class="rounded my-thumb" src="./img/project/2_F.png" alt="Card image cap"> Rovereatz'
+    );
+    $('img[class="d-block w-100"]').each(function (index, element) {
+      $(element).attr('src', `./img/gif/rovereatz/${index + 1}.png`);
+    });
+    $('.modal-body .content').text(
       'A Travelogue. B2B & B2C type social platform connecting nomads, food freaks & the common. Techstack involved: Spring, Mongo DB,ElasticSearch, Bootstrap, React.'
     );
-    $('#github_uri').html('<i class="fab fa-github"> </i> Github');
+    $('#github_uri').html('<i class="fab fa-gitlab"> </i> Gitlab');
     $('#github_uri').attr('href', 'https://gitlab.com/aravinve/rovereatz');
-    $('#demo_uri').html('<i class="fas fa-eye"></i> Live Demo');
-    $('#demo_uri').attr('href', '#!');
   });
   $('#bingebuffer').click(function () {
-    $('#projectModalLabel').text('Bingebuffer');
-    $('.modal-body').text(
+    $('#projectModalLabel').html(
+      ' <img class="rounded my-thumb" src="./img/project/3_F.png" alt="Card image cap"> Bingebuffer'
+    );
+    $('img[class="d-block w-100"]').each(function (index, element) {
+      $(element).attr('src', `./img/gif/binge-buffer/${index + 1}.png`);
+    });
+    $('.modal-body .content').text(
       'An unbiased IMDB type webapp built with Django & Materialize. Moderator/Public type review platform for movies, anime, series & games.'
     );
     $('#github_uri').html('<i class="fab fa-github"> </i> Github');
     $('#github_uri').attr('href', 'https://github.com/aravinve/bingebuffer');
-    $('#demo_uri').html('<i class="fas fa-eye"></i> Live Demo');
-    $('#demo_uri').attr('href', '#!');
   });
   $('#uob').click(function () {
-    $('#projectModalLabel').text('UOB One Design');
-    $('.modal-body').text(
+    $('#projectModalLabel').html(
+      '<img class="rounded my-thumb" src="./img/project/4_F.png" alt="Card image cap"> UOB One Design'
+    );
+    $('img[class="d-block w-100"]').each(function (index, element) {
+      $(element).attr('src', `./img/gif/uob/${index + 1}.png`);
+    });
+    $('.modal-body .content').text(
       'A Redefined Design Approach for United Overseas Bank (UOB). Structured Digital Innovation of services such as Member Onboarding, CRM, Merchant Services in the spheres of service process and design innovation. Used Figma And Materialize Design Standards'
     );
-    $('#github_uri').html('<i class="fas fa-eye"></i> Live Demo - UOB One');
+    $('#github_uri').html('<i class="fab fa-figma"></i> Live Demo - UOB One');
     $('#github_uri').attr(
       'href',
       'https://www.figma.com/proto/Az8bzCYpxBy81pr2lFiekH/UOB-DI?node-id=2%3A34&scaling=scale-down'
     );
-    $('#demo_uri').html('<i class="fas fa-eye"></i> Live Demo - UOB CRM ');
-    $('#demo_uri').attr(
-      'href',
-      'https://www.figma.com/proto/Az8bzCYpxBy81pr2lFiekH/UOB-DI?node-id=53%3A438&scaling=contain'
-    );
   });
   $('#hangar').click(function () {
-    $('#projectModalLabel').text('Hangar');
-    $('.modal-body').text(
+    $('#projectModalLabel').html(
+      ' <img class="rounded my-thumb" src="./img/project/5_F.png" alt="Card image cap">Hangar'
+    );
+    $('img[class="d-block w-100"]').each(function (index, element) {
+      $(element).attr('src', `./img/gif/hangar/${index + 1}.png`);
+    });
+    $('.modal-body .content').text(
       "A multipurpose desktop application built using Electron and Bulma along with the integration of awesome REST API's."
     );
     $('#github_uri').html('<i class="fab fa-github"> </i> Github');
     $('#github_uri').attr('href', 'https://github.com/aravinve/hangar');
-    $('#demo_uri').html('<i class="fas fa-eye"></i> Live Demo');
-    $('#demo_uri').attr('href', '#!');
   });
   $('#fcc').click(function () {
-    $('#projectModalLabel').text('FCC Projects');
-    $('.modal-body').text(
+    $('#projectModalLabel').html(
+      '<img class="rounded my-thumb" src="./img/project/6_F.png" alt="Card image cap"> FCC Projects'
+    );
+    $('img[class="d-block w-100"]').each(function (index, element) {
+      $(element).attr('src', `./img/gif/fcc-projects/${index + 1}.png`);
+    });
+    $('.modal-body .content').text(
       ' A dedicated website for FreeCodeCamp projects. Techstack involves HTML5, CSS, Javascript and Materialize.'
     );
     $('#github_uri').html('<i class="fab fa-github"> </i> Github');
     $('#github_uri').attr('href', 'https://github.com/aravinve/fcc-projects');
-    $('#demo_uri').html('<i class="fas fa-eye"></i> Live Demo');
-    $('#demo_uri').attr('href', '#!');
   });
 })(jQuery); // End of use strict
